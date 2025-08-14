42cb92504c306fb77b8f97b43a0f2c32ab450c7d
XL Software <dev@xlsoftware.ru>
2025-08-14 20:58:47 +0300

    feat: export all types

--- src/mod.ts
+++ src/mod.ts
@@ -2,5 +2,14 @@ import { defineTask, TaskEvent } from "./lib/defineTask.ts";
 import { useTasks, event } from "./lib/useTasks.ts";
 
 export {
-    defineTask, useTasks, event, TaskEvent
+    defineTask, TaskEvent,
+    useTasks, event,
+}
+
+import type { Task, TaskWorker, TaskEventInterface, TaskTrigger, TaskOptions, TaskModule } from "./types/task.types.ts"
+import type { Log } from "./types/log.types.ts";
+
+export type {
+    Task, TaskWorker, TaskEventInterface, TaskTrigger, TaskOptions, TaskModule,
+    Log,
 }
